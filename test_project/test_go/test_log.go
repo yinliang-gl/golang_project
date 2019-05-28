a586b04fd2efd95336258e65d818285b0e0db06b
尹亮 <yinliang@qutoutiao.net>
2019-05-28 14:33:17 +0800

    dfafdsfsd

---
+++ test_project/test_go/test_log.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+// 简单输出
+func test_1() {
+	arr := []int{2, 3}
+	log.Print("Print array ", arr, "\n")
+	log.Println("Println array", arr)
+	log.Printf("Printf array with item [%d,%d]\n", arr[0], arr[1])
+}
+
+/**
+测试 Fatal
+对于 log.Fatal 接口，会先将日志内容打印到标准输出，接着调用系统的 os.exit(1) 接口，退出程序并返回状态 1 。
+但是有一点需要注意，由于是直接调用系统接口退出，defer函数不会被调用
+*/
+func test_2() {
+	defer func() {
+		fmt.Println("--first--")
+	}()
+	log.Fatalln("test for defer Fatal")
+}
+
+/**
+测试 Panic
+可以看到首先输出了“test for defer Panic”，然后第一个defer函数被调用了并输出了“--first--”，
+但是第二个defer 函数并没有输出，可见在Panic之后声明的defer是不会执行的。
+*/
+func test_3() {
+
+	defer func() {
+		fmt.Println("--first--")
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	log.Panicln("test for defer Panic")
+	defer func() {
+		fmt.Println("--second--")
+	}()
+
+}
+
+/**
+
+ */
+func test_4() {
+	fileName := "test_project/test_go/Info_First.log"
+	logFile, err := os.Create(fileName)
+	defer logFile.Close()
+	if err != nil {
+		log.Fatalln("open file error")
+	}
+	debugLog := log.New(logFile, "[Info]", log.Llongfile)
+	debugLog.Println("A Info message here")
+	debugLog.SetPrefix("[Debug]")
+	debugLog.Println("A Debug Message here ")
+}
+func main() {
+	// test_1()
+	// test_2()
+	//test_3()
+	test_4()
+}
