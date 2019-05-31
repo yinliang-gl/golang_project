f4636c648938d7ae48793f367c635e6d2639715d
尹亮 <yinliang@qutoutiao.net>
2019-05-31 15:17:05 +0800

    dfafdsfsd

---
+++ test_project/test_go/test_time.go
@@ -0,0 +1,221 @@
+package main
+
+/**
+参考 https://blog.csdn.net/wschq/article/details/80114036
+*/
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+/**
+初始化
+*/
+func test_time_1() {
+	// func Now() Time
+	fmt.Println(time.Now())
+
+	// func Parse(layout, value string) (Time, error)
+	time.Parse("2016-01-02 15:04:05", "2018-04-23 12:24:51")
+
+	// func ParseInLocation(layout, value string, loc *Location) (Time, error) (layout已带时区时可直接用Parse)
+	time.ParseInLocation("2006-01-02 15:04:05", "2017-05-11 14:06:06", time.Local)
+
+	// func Unix(sec int64, nsec int64) Time
+	time.Unix(1e9, 0)
+
+	// func Date(year int, month Month, day, hour, min, sec, nsec int, loc *Location) Time
+	time.Date(2018, 1, 2, 15, 30, 10, 0, time.Local)
+
+	// func (t Time) In(loc *Location) Time 当前时间对应指定时区的时间
+	loc, _ := time.LoadLocation("America/Los_Angeles")
+	fmt.Println(time.Now().In(loc))
+}
+
+/**
+格式化和常用方法
+
+*/
+func test_time_2() {
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05")) // 2018-04-24 10:11:20
+	fmt.Println(time.Now().Format(time.UnixDate))         // Tue Apr 24 09:59:02 CST 2018
+
+	fmt.Println(time.Now().Unix())
+
+	// 获取指定日期的时间戳
+	dt, _ := time.Parse("2016-01-02 15:04:05", "2018-04-23 12:24:51")
+	fmt.Println(dt.Unix())
+
+	fmt.Println(time.Date(2018, 1, 2, 15, 30, 10, 0, time.Local).Unix())
+}
+
+/*
+时间段(Duartion)
+
+      func (d Duration) Hours() float64
+      func (d Duration) Minutes() float64
+      func (d Duration) Seconds() float64
+      func (d Duration) Nanoseconds() int64
+      func (d Duration) Round(m Duration) Duration         // 四舍五入
+      func (d Duration) Truncate(m Duration) Duration      // 向下取整
+*/
+func test_time_3() {
+	tp, _ := time.ParseDuration("1.5s")
+	fmt.Println(tp.Truncate(1000), tp.Seconds(), tp.Nanoseconds())
+}
+
+/*
+时区(Location)
+*/
+func test_time_4() {
+	// 默认UTC
+	_, _ = time.LoadLocation("")
+	// 服务器设定的时区，一般为CST
+	_, _ = time.LoadLocation("Local")
+	// 美国洛杉矶PDT
+	_, _ = time.LoadLocation("America/Los_Angeles")
+
+	// 获取指定时区的时间点
+	local, _ := time.LoadLocation("America/Los_Angeles")
+	fmt.Println(time.Date(2018, 1, 1, 12, 0, 0, 0, local))
+
+}
+
+/*
+时间运算
+  func Until(t Time) Duration     //  等价于 t.Sub(Now())，t与当前时间的间隔
+*/
+func test_time_5() {
+
+	// func After(d Duration) <-chan Time  非阻塞,可用于延迟
+	time.After(time.Duration(10) * time.Second)
+
+	// func Since(t Time) Duration 两个时间点的间隔
+	start := time.Now()
+
+	// func Sleep(d Duration)   休眠多少时间，休眠时处于阻塞状态，后续程序无法执行
+	time.Sleep(time.Duration(10) * time.Second)
+
+	fmt.Println(time.Since(start)) // 等价于 Now().Sub(t)， 可用来计算一段业务的消耗时间
+
+	// func (t Time) Add(d Duration) Time
+	fmt.Println(start.Add(time.Duration(10) * time.Second)) // 加
+	// func (t Time) Sub(u Time) Duration                    // 减
+
+	// func (t Time) AddDate(years int, months int, days int) Time
+	fmt.Println(start.AddDate(1, 1, 1))
+
+	// func (t Time) Before(u Time) bool
+	// func (t Time) After(u Time) bool
+	// func (t Time) Equal(u Time) bool          比较时间点时尽量使用Equal函数
+
+	dt1 := time.Date(2018, 1, 10, 0, 0, 1, 100, time.Local)
+	dt2 := time.Date(2018, 1, 9, 23, 59, 22, 100, time.Local)
+	// 不用关注时区，go会转换成时间戳进行计算
+	fmt.Println(dt1.Sub(dt2))
+
+	now := time.Now()
+
+	// 一段时间之后
+	fmt.Println(now.Add(time.Duration(10) * time.Minute))
+
+	// 计算两个时间点的相差天数
+	dt1 = time.Date(dt1.Year(), dt1.Month(), dt1.Day(), 0, 0, 0, 0, time.Local)
+	dt2 = time.Date(dt2.Year(), dt2.Month(), dt2.Day(), 0, 0, 0, 0, time.Local)
+	fmt.Println(int(math.Ceil(dt1.Sub(dt2).Hours() / 24)))
+
+}
+
+/*
+时区转换
+*/
+func test_time_6() {
+	// time.Local 用来表示当前服务器时区
+	// 自定义地区时间
+	secondsEastOfUTC := int((8 * time.Hour).Seconds())
+	beijing := time.FixedZone("Beijing Time", secondsEastOfUTC)
+	fmt.Println(time.Date(2018, 1, 2, 0, 0, 0, 0, beijing)) // 2018-01-02 00:00:00 +0800 Beijing Time
+
+	// 当前时间转为指定时区时间
+	fmt.Println(time.Now().In(beijing))
+
+	// 指定时间转换成指定时区对应的时间
+	_, _ = time.ParseInLocation("2006-01-02 15:04:05", "2017-05-11 14:06:06", time.Local)
+
+	// 当前时间在零时区年月日   时分秒  时区
+	_, _, _ = time.Now().UTC().Date()  // 2018 April 24
+	_, _, _ = time.Now().UTC().Clock() // 3 47 15
+	_, _ = time.Now().UTC().Zone()     // UTC
+
+}
+
+/*
+比较两个时间点
+*/
+
+func test_time_7() {
+	dt := time.Date(2018, 1, 10, 0, 0, 1, 100, time.Local)
+	fmt.Println(time.Now().After(dt))  // true
+	fmt.Println(time.Now().Before(dt)) // false
+
+	// 是否相等 判断两个时间点是否相等时推荐使用 Equal 函数
+	fmt.Println(dt.Equal(time.Now()))
+}
+
+/*
+设置执行时间，超时限制等
+*/
+func test_time_8() {
+	c := make(chan interface{})
+	select {
+	case m := <-c:
+		fmt.Println(m)
+	case <-time.After(time.Duration(5) * time.Second):
+		fmt.Println("time out")
+	}
+}
+
+/*
+Ticker类型
+有时我们会遇到每隔一段时间执行的业务(比如设置心跳时间等)，就可以用它来处理，这是一个重复的过程
+*/
+func test_time_9_1() {
+	// 无法取消
+	//i := 0
+	//tick := time.Tick(1 * time.Second)
+	//for _ = range tick {
+	//	// do something
+	//	fmt.Println("i = ", i)
+	//	i = i + 1
+	//}
+
+}
+func test_time_9_2() {
+	// 可通过调用ticker.Stop取消
+	i := 0
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for _ = range ticker.C {
+		if i == 5 {
+			return
+		}
+		fmt.Println("i = ", i)
+		i = i + 1
+		// do something
+	}
+}
+
+func main() {
+	// test_time_1()
+	// test_time_2()
+	// test_time_3()
+	// test_time_4()
+	// test_time_5()
+	// test_time_6()
+	// test_time_7()
+	// test_time_8()
+	// test_time_9_1()
+	test_time_9_2()
+}
---
+++ test_project/test_go/test_timer_ticker.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+/*
+timer
+*/
+func test_timer_ticker_1() {
+
+	input := make(chan interface{})
+
+	//producer - produce the messages
+	go func() {
+		for i := 0; i < 100; i++ {
+			input <- i
+			time.Sleep(time.Duration(1) * time.Second)
+		}
+		input <- "hello, world"
+	}()
+
+	t1 := time.NewTimer(time.Second * 5)
+	t2 := time.NewTimer(time.Second * 10)
+
+	for {
+		select {
+		//consumer - consume the messages
+		case msg := <-input:
+			fmt.Println(msg)
+
+		case <-t1.C:
+			println("5s timer")
+			t1.Reset(time.Second * 5)
+
+		case <-t2.C:
+			println("10s timer")
+			t2.Reset(time.Second * 10)
+		}
+	}
+}
+
+/**
+*ticker只要定义完成，从此刻开始计时，不需要任何其他的操作，每隔固定时间都会触发。
+*timer定时器，是到固定时间后会执行一次
+*如果timer定时器要每隔间隔的时间执行，实现ticker的效果，使用 func (t *Timer) Reset(d Duration) bool
+ */
+func test_timer_ticker_2() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	//NewTimer 创建一个 Timer，它会在最少过去时间段 d 后到期，向其自身的 C 字段发送当时的时间
+	timer1 := time.NewTimer(2 * time.Second)
+
+	//NewTicker 返回一个新的 Ticker，该 Ticker 包含一个通道字段，并会每隔时间段 d 就向该通道发送当时的时间。它会调
+	//整时间间隔或者丢弃 tick 信息以适应反应慢的接收者。如果d <= 0会触发panic。关闭该 Ticker 可
+	//以释放相关资源。
+	ticker1 := time.NewTicker(2 * time.Second)
+
+	go func(t *time.Ticker) {
+		defer wg.Done()
+		for {
+			<-t.C
+			fmt.Println("get ticker1", time.Now().Format("2006-01-02 15:04:05"))
+		}
+	}(ticker1)
+
+	go func(t *time.Timer) {
+		defer wg.Done()
+		for {
+			<-t.C
+			fmt.Println("get timer", time.Now().Format("2006-01-02 15:04:05"))
+			//Reset 使 t 重新开始计时，（本方法返回后再）等待时间段 d 过去后到期。如果调用时t
+			//还在等待中会返回真；如果 t已经到期或者被停止了会返回假。
+			t.Reset(2 * time.Second)
+		}
+	}(timer1)
+
+	wg.Wait()
+}
+func main() {
+	// test_timer_ticker_1()
+	test_timer_ticker_2()
+}
