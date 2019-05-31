7caf59e64f791c6a2c4c9e30ff742b1c23e93035
尹亮 <yinliang@qutoutiao.net>
2019-05-31 19:04:45 +0800

    dfafdsfsd

--- test_project/test_go/test_map.go
+++ test_project/test_go/test_map.go
@@ -3,19 +3,6 @@ package main
 import "fmt"
 
 type MaterialResource struct {
-	ID          int32
-	Type        int32
-	FileName    string
-	RemoteURL   string
-	FileSize    int64
-	ImageWidth  int32
-	ImageHeight int32
-	AppName     string
-	AppPackage  string
-	AppLogo     int32
-	AppVersion  string
-	FileMd5     string
-	Duration    uint32
 }
 
 func main() {
