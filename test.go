f29d60561a50baece0378fc57b984285d5b7055c
yinliang <yinliang@qutoutiao.net>
2019-09-03 16:01:47 +0800

    fsafsdds

--- test.go
+++ test.go
@@ -1,8 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"git.qutoutiao.net/CPC/data_bus/tools/golang_tools/proto"
+	"git.qutoutiao.net/CPC/data_bus/tools/golang_tools/util"
+	"github.com/gogo/protobuf/proto"
+	"runtime"
+)
 
 func main() {
-	aaa := make([]int, 0, 10)
-	fmt.Println(aaa)
+	benchmark_file_util := data_bus.BenchmarkFileUtil{}
+	if !benchmark_file_util.Init("/Users/qtt/Desktop/tmp/material.data") {
+		fmt.Errorf("Init error")
+	}
+
+	for {
+		if success, result := benchmark_file_util.Next(); success == true {
+			var material model_algorithm.Material
+			proto.Unmarshal([]byte(result), &material)
+			//fmt.Println(material.String())
+			//fmt.Println(result)
+		} else {
+			break
+		}
+	}
+
+	fmt.Printf("\n\n\nGetTotalItemCount[%d]\n", benchmark_file_util.GetTotalItemCount())
+
+	runtime.GC()
 }
