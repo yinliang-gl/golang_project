d2db0a92e1d1596a4b9667887d361a3beb1bc4fe
yinliang <yinliang@qutoutiao.net>
2019-10-21 18:15:18 +0800

    step

--- test_project/test_go_convey/aaa.go
+++ test_project/test_go_convey/aaa.go
@@ -1,24 +1,22 @@
 package test_go_convey
 
-
 import (
 	"testing"
-	. "github.com/smartystreets/goconvey"
+	//. "github.com/smartystreets/goconvey"
 )
 
 func TestSpec(t *testing.T) {
 
-	// Only pass t into top-level Convey calls
-	Convey("Given some integer with a starting value", t, func() {
-		x := 1
-
-		Convey("When the integer is incremented", func() {
-			x++
-
-			Convey("The value should be greater by one", func() {
-				So(x, ShouldEqual, 2)
-			})
-		})
-	})
+	//// Only pass t into top-level Convey calls
+	//Convey("Given some integer with a starting value", t, func() {
+	//	x := 1
+	//
+	//	Convey("When the integer is incremented", func() {
+	//		x++
+	//
+	//		Convey("The value should be greater by one", func() {
+	//			So(x, ShouldEqual, 2)
+	//		})
+	//	})
+	//})
 }
-
