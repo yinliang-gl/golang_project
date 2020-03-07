5d255ae4f7c4724dcb31040298a066f0c066bcb0
yinliang <yinliang@qutoutiao.net>
2020-03-07 17:31:20 +0800

    add test_log

--- test_project/test_fasthttp/demo1_client/main.go
+++ test_project/test_fasthttp/demo1_client/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/valyala/fasthttp"
+	"time"
 )
 
-// 标准的 client
+// 标准的 client （充分使用复用技术）
 // 参考   https://studygolang.com/articles/17716
 func main() {
 	url := `http://httpbin.org/post?key=123`
@@ -25,7 +26,7 @@ func main() {
 	resp := fasthttp.AcquireResponse()
 	defer fasthttp.ReleaseResponse(resp) // 用完需要释放资源
 
-	if err := fasthttp.Do(req, resp); err != nil {
+	if err := fasthttp.DoTimeout(req, resp, 5*time.Second); err != nil {
 		fmt.Println("请求失败:", err.Error())
 		return
 	}
