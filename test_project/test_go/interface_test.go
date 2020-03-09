8d9a6a419c009a7e09e83532e1a4118a7f88920e
yinliang <yinliang@qutoutiao.net>
2020-03-09 13:50:09 +0800

    add test_log

--- test_project/test_go/interface_test.go
+++ test_project/test_go/interface_test.go
@@ -1,6 +1,7 @@
 package test_go
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 )
@@ -37,7 +38,7 @@ func (this *DivModule) Run(a int, b int) (result int) {
 	return a / b
 }
 
-func TestAAA(t *testing.T) {
+func TestInterface01(t *testing.T) {
 	var algorithmModules []AlgorithmModule
 
 	algorithmModules = append(algorithmModules, &AddModule{})
@@ -49,4 +50,31 @@ func TestAAA(t *testing.T) {
 		fmt.Println(algorithmModules[i].Run(100, 90))
 	}
 
+}
+
+func GetModule(module_name string) (Algor AlgorithmModule, err error) {
+	if module_name == "add" {
+		return &AddModule{}, nil
+	} else if module_name == "sub" {
+		return &SubModule{}, nil
+	} else {
+		return nil, errors.New("unsupport module:" + module_name)
+	}
+}
+
+func TestInterface02(t *testing.T) {
+	var moduleNames []string
+	moduleNames = append(moduleNames, "add")
+	moduleNames = append(moduleNames, "sub")
+	moduleNames = append(moduleNames, "aaa")
+
+	for i := 0; i < len(moduleNames); i++ {
+		module, err := GetModule(moduleNames[i])
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+		fmt.Printf("%s, %d\n", moduleNames[i], module.Run(100, 90))
+		// fmt.Println(GetModule(moduleNames[i]).Run(100, 90))
+	}
 }
