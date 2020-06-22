abcedc393c85ade3953edb323169106a2cfd8d1b
yinliang <yinliang@qutoutiao.net>
2020-06-22 14:13:23 +0800

    add test_log

--- test_project/test_md5/main.go
+++ test_project/test_md5/main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
-	"crypto/md5"
+	"encoding/base64"
 	"fmt"
+	"log"
 )
 
 func main() {
-	println("fasfasd")
+	input := []byte("hello world")
 
-	androidId := "devId.ID"
-	androidMd5 := fmt.Sprintf("%x", md5.Sum([]byte(androidId)))
+	// 演示base64编码
+	encodeString := base64.StdEncoding.EncodeToString(input)
+	fmt.Println(encodeString)
 
-	println(androidMd5)
+	// 对上面的编码结果进行base64解码
+	decodeBytes, err := base64.StdEncoding.DecodeString(encodeString)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(string(decodeBytes))
+
+	fmt.Println("///////////////////////////////")
+
+	// 如果要用在url中，需要使用URLEncoding
+	uEnc := base64.URLEncoding.EncodeToString([]byte(input))
+	fmt.Println(uEnc)
+
+	uDec, err := base64.URLEncoding.DecodeString(uEnc)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(string(uDec))
 }
