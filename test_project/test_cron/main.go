153790b5580f1e464990c826aca900b950d40680
yinliang <yinliang@qutoutiao.net>
2020-10-15 11:09:36 +0800

    step

--- test_project/test_cron/main.go
+++ test_project/test_cron/main.go
@@ -1,47 +1,30 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
+	"github.com/robfig/cron/v3"
+	"log"
 	"time"
 )
 
-type AppPackageFreqFilter struct {
-	needFilter      		bool
-	timeLimitOfPutType1 	int64
-	timeLimitOfPutType2 	int64
-	currentSeconds			int64
-}
-
-func (apf *AppPackageFreqFilter) Init(tmpStr string) {
-	puttypeStr := strings.Split(tmpStr, ":")
-	for _, itemStr := range puttypeStr {
-		subTmpStr := strings.Split(itemStr, ",")
-		if len(subTmpStr) != 2 {
-			continue
-		}
-
-		valInt, err := strconv.Atoi(subTmpStr[1])
-		if err != nil {
-			continue
-		}
-
-		if len(subTmpStr[0]) == 0 {
-			continue
-		}
-		if subTmpStr[0][0] == '1' {
-			apf.timeLimitOfPutType1 = int64(valInt)
-		} else if subTmpStr[0][0] == '2' {
-			apf.timeLimitOfPutType2 = int64(valInt)
-		}
-	}
-	apf.currentSeconds = time.Now().Unix()
-	// adslog.Debug("yinliang_AppPackageFreqFilter tmpStr:%#v, apf.needFilter:%#v, apf.timeLimitOfPutType1:%#v, apf.timeLimitOfPutType2:%#v", tmpStr, apf.needFilter, apf.timeLimitOfPutType1, apf.timeLimitOfPutType2)
-}
-
 func main() {
-	appPackageFreqFilter := &AppPackageFreqFilter{}
-	appPackageFreqFilter.Init("1,60:2,600")
-	fmt.Println(appPackageFreqFilter.timeLimitOfPutType2)
+	i := 0
+	c := cron.New()
+	spec := "*/1 * * * * ?"
+	c.AddFunc(spec, func() {
+		i++
+		log.Println("cron running i = ", i)
+	})
+	c.Start()
+
+	time.Sleep(10 * time.Second)
+
+	log.Println("new function")
+
+	j:=0
+	c.AddFunc(spec, func() {
+		j++
+		log.Println("cron running j = ", j)
+	})
+
+	select{}
 }
--- test_project/test_json/main.go
+++ test_project/test_json/main.go
@@ -9,6 +9,44 @@ import (
 	"fmt"
 )
 
+type OperatorItem struct {
+	Key       uint64  `json:"key"`
+	Op        string  `json:"op"`
+	Value     []int64 `json:"value"`
+	Threshold string  `json:"threshold"`
+}
+
+type OperatorList struct {
+	operatorItems []OperatorItem `json:"list"`
+}
+
+func main() {
+	var err error
+
+
+
+
+
+
+
+	str1 := `{
+      "key": "budget",
+      "op": "inc",
+      "value": [10],
+      "thres": 0
+    }`
+
+	var OperatorItem OperatorItem
+	err = json.Unmarshal([]byte(str1), &OperatorItem)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%#v\n", OperatorItem)
+
+	return
+
+}
+
 type UsergrowthDhhDeliveryAskResponse struct {
 	Result  bool
 	Task_id string
@@ -27,7 +65,7 @@ type TaobaoRtaResponse struct {
 	Error_response                       ErrorResponse
 }
 
-func main() {
+func main1() {
 	var s TaobaoRtaResponse
 	//str := `{"usergrowth_dhh_delivery_ask_response":
 	//{"result":true,"task_id":"123456","errcode":1},"error_response":
@@ -45,4 +83,118 @@ func main() {
 	}
 	fmt.Println(s)
 	// 	fmt.Println(s.Servers[0].ServerName)
+}
+
+type RuleItem struct {
+	Level       string  `json:"level"`
+	DayInterval int     `json:"day_interval"`
+	Key         string  `json:"key"`
+	Op          string  `json:"op"`
+	Value       []int64 `json:"value"`
+}
+
+type RuleAnd struct {
+	Rules []RuleItem `json:"rules"`
+}
+
+type RuleAndOr struct {
+	RuleAnds []RuleAnd `json:"list"`
+}
+
+func main2() {
+	var str string
+	var err error
+
+	var s RuleAndOr
+
+	str = `{
+  "list": [
+    {
+      "rules": [
+        {
+          "level": "user",
+          "day_interval": 1,
+          "key": "inner_realtime_active",
+          "op": "gt",
+          "value": [
+            100
+          ]
+        },
+        {
+          "level": "group",
+          "day_interval": 1,
+          "key": "inner_realtime_active",
+          "op": "gt",
+          "value": [
+            10
+          ]
+        }
+      ]
+    },
+    {
+      "rules": [
+        {
+          "level": "user",
+          "day_interval": 1,
+          "key": "inner_realtime_active",
+          "op": "gt",
+          "value": [
+            100
+          ]
+        }
+      ]
+    }
+  ]
+}`
+	err = json.Unmarshal([]byte(str), &s)
+	if err != nil {
+		fmt.Printf("%#v", s)
+	}
+	// fmt.Println(s)
+	fmt.Printf("%#v", s)
+
+	return
+
+	str = `{
+    "rules": [
+      {
+        "level": "user",
+        "day_interval": 1,
+        "key": "inner_realtime_active",
+        "op": "gt",
+        "value": [
+          100
+        ]
+      },
+      {
+        "level": "group",
+        "day_interval": 1,
+        "key": "inner_realtime_active",
+        "op": "gt",
+        "value": [
+          10
+        ]
+      }
+    ]
+  }`
+
+	var ruleAnd RuleAnd
+	err = json.Unmarshal([]byte(str), &ruleAnd)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%#v\n", ruleAnd)
+	fmt.Printf("%#v\n", len(ruleAnd.Rules))
+
+	return
+
+	str = `{"level":"user","day_interval":1,"key":"inner_realtime_active","op":"gt","value":[100]}`
+	var item RuleItem
+	err = json.Unmarshal([]byte(str), &item)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%#v", item)
+	return
+
 }
