26393b3aa372e7da5be1e4679c30e70bd876a39c
yinliang <yinliang@qutoutiao.net>
2021-03-16 17:41:10 +0800

    step

--- test.go
+++ test.go
@@ -2,115 +2,13 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"time"
 )
 
-var TOTAL_NUM int = 100000000
-
-func func1() {
-
-	elementMap := make(map[string]int)
-	slotIdStr := "999999999"
-	groupIdStr := "88888888"
-	for i := 0; i < TOTAL_NUM; i++ {
-		elementMap[slotIdStr+groupIdStr+strconv.Itoa(i)] = 1
-	}
-
-	var arr []string
-	for i := 0; i < TOTAL_NUM; i++ {
-		arr = append(arr, strconv.Itoa(i))
-	}
-	bT := time.Now() // 开始时间
-	for i := 0; i < TOTAL_NUM; i++ {
-		_, find := elementMap[slotIdStr+groupIdStr+arr[i%TOTAL_NUM]]
-		if find {
-
-		}
-	}
-	eT := time.Since(bT) // 从开始到当前所消耗的时间
-	fmt.Println("Run time: ", eT)
-}
-
-type DictV2Key struct {
-	slotIdStr  int
-	groupIdStr int
-	valInt     int
-}
-
-func func2() {
-
-	elementMap := make(map[DictV2Key]int)
-	for i := 0; i < TOTAL_NUM; i++ {
-		tmpDictKey := DictV2Key{
-			slotIdStr:  999999999,
-			groupIdStr: 88888888,
-			valInt:     i,
-		}
-		elementMap[tmpDictKey] = 1
-	}
-
-	var arr []int
-	for i := 0; i < TOTAL_NUM; i++ {
-		arr = append(arr, i)
-	}
-	bT := time.Now() // 开始时间
-	for i := 0; i < TOTAL_NUM; i++ {
-		tmpDictKey := DictV2Key{
-			slotIdStr:  i * 999,
-			groupIdStr: i * 88,
-			valInt:     arr[i%TOTAL_NUM],
-		}
-		_, find := elementMap[tmpDictKey]
-		if find {
-
-		}
-	}
-
-	eT := time.Since(bT) // 从开始到当前所消耗的时间
-	fmt.Println("Run time: ", eT)
-}
-
-type DictV3Key struct {
-	ElementString [3]string
-	ElementInt    [3]int64
-}
-
-func (this *DictV3Key) Clear() {
-	var tmpStr [3]string
-	this.ElementString = tmpStr
-
-	var tmpInt [3]int64
-	this.ElementInt = tmpInt
-}
-func func3() {
-	elementMap := make(map[DictV3Key]int)
-	for i := 0; i < TOTAL_NUM; i++ {
-		tmpDictKey := DictV3Key{
-			// ElementString: [3]string{slotIdStr, groupIdStr},
-			ElementInt: [3]int64{int64(i), 3, 3},
-		}
-		elementMap[tmpDictKey] = 1
-	}
-
-	bT := time.Now() // 开始时间
-	for i := 0; i < TOTAL_NUM; i++ {
-		tmpDictKey := DictV3Key{
-			// ElementString: [3]string{slotIdStr, groupIdStr},
-			ElementInt: [3]int64{int64(i), 3, 3},
-		}
-		_, find := elementMap[tmpDictKey]
-		if find {
-
-		}
-	}
-
-	eT := time.Since(bT) // 从开始到当前所消耗的时间
-	fmt.Println("Run time: ", eT)
-}
-
 func main() {
-	func1()
-	func2()
-	func3()
+	timeStr := time.Now().Format("2006-01-02 15:04:05")
+	fmt.Println(timeStr)
+
+	timeStr = string([]byte(timeStr)[11:])
+	fmt.Println(timeStr)
 }
--- test_project/test_excel/main.go
+++ test_project/test_excel/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/tealeg/xlsx"
+	"strconv"
+	"strings"
 )
 
 type RunInfos struct {
+	AdIds          []int64
 	ImageStartTime string
 	ImageEndTime   string
 	VideoStartTime string
@@ -14,32 +17,75 @@ type RunInfos struct {
 }
 
 type TitleEntity struct {
-	AllString map[string][]string
+	AllString map[string]string
 }
 
-func readXlsx(filename string) []RunInfos {
-	var listOra []RunInfos
+func readXlsx(filename string) RunInfos {
+	var supportCreativeTypeMap map[string][]int64
+	supportCreativeTypeMap["广点通-横版大图&横版视频&竖版视频"] = append(supportCreativeTypeMap["广点通-横版大图&横版视频&竖版视频"], 100)
+
+	var runInfos RunInfos
 	xlFile, err := xlsx.OpenFile(filename)
 	if err != nil {
 		fmt.Printf("open failed: %s\n", err)
 	}
+
 	fmt.Printf("length of xlFile.Sheets is %v\n", len(xlFile.Sheets))
 	for _, sheet := range xlFile.Sheets {
 		fmt.Printf("Sheet Name: %s\n", sheet.Name)
-		tmpOra := RunInfos{}
-		for _, row := range sheet.Rows {
-			var strs []string
-			for _, cell := range row.Cells {
-				text := cell.String()
-				strs = append(strs, text)
+
+		var supportCreativeType []int64
+
+		if sheet.Name == "汇总信息" {
+			FillSummaryInformation(sheet, runInfos)
+			continue
+		} else {
+			var find bool
+			if supportCreativeType, find = supportCreativeTypeMap[sheet.Name]; !find {
+				fmt.Errorf("不支持的类型[%v]", sheet.Name)
+				continue
 			}
-			fmt.Println("================================")
-			fmt.Println(sheet.Name)
-			fmt.Println(strs)
-			listOra = append(listOra, tmpOra)
+			var titleEntitys []TitleEntity
+
+			for i := 1; i < len(sheet.Rows); i++ {
+				for j := 0; j < len(sheet.Rows[i].Cells); j++ {
+
+				}
+			}
+
+			for _, val := range supportCreativeType {
+				runInfos.TitleEntices[val] = titleEntitys
+			}
+
 		}
 	}
-	return listOra
+	return runInfos
+}
+
+func FillSummaryInformation(sheet *xlsx.Sheet, runInfos RunInfos) {
+	if len(sheet.Rows) != 2 {
+		panic("汇总信息格式错误")
+	}
+	if len(sheet.Rows[1].Cells) != 5 {
+		panic("汇总、" +
+			"信息格式错误")
+	}
+	// 1. 模板广告id
+	templateIdsStr := sheet.Rows[1].Cells[0].String()
+	templateIdsStrSplit := strings.Split(templateIdsStr, ",")
+	for _, adidStr := range templateIdsStrSplit {
+		adId, err := strconv.ParseInt(adidStr, 10, 64)
+		if err != nil {
+			panic("模板id转为int64失败" + adidStr)
+		}
+		runInfos.AdIds = append(runInfos.AdIds, adId)
+	}
+	// 2. 图片开始结束时间
+	runInfos.ImageStartTime = sheet.Rows[1].Cells[1].String()
+	runInfos.ImageEndTime = sheet.Rows[1].Cells[2].String()
+	// 3. 视频开始结束时间
+	runInfos.ImageStartTime = sheet.Rows[1].Cells[1].String()
+	runInfos.ImageEndTime = sheet.Rows[1].Cells[2].String()
 }
 
 func main() {
@@ -47,5 +93,5 @@ func main() {
 		"postman请求-15-18814682-正保科技-中级会计师-微信/广点通_账户id_2021_01_22_15_02.xlsx"
 	oraList := readXlsx(excelFileName)
 
-	fmt.Println("%#v", oraList)
+	fmt.Printf("%#v", oraList)
 }
